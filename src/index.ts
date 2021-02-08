91633ee3cdbb9ef994e2892a57db5034ec684688
Zohra <laid.zohra@gmail.com>
2021-02-08 14:42:54 +0100

    bon setup branch dev

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,9 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-    res.send('Hello World!')
-})
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-=======
   res.send('Hello World!')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
->>>>>>> 1c258fd74ef509ded9ff5b39deafbb44d37d92d9
 })
--- src/service/config.service.ts
+++ src/service/config.service.ts
@@ -1,10 +1,6 @@
 export default class ConfigService {
 
-<<<<<<< HEAD
     init: () => {
-=======
-    init: ()=> {
->>>>>>> 1c258fd74ef509ded9ff5b39deafbb44d37d92d9
 
     }
 }
