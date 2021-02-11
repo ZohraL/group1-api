b7225a1f15d47004f6a1341187b0fe7dc9416f61
Zohra <laid.zohra@gmail.com>
2021-02-11 11:46:50 +0100

    PLEASE

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv = require("dotenv");
 const express = require("express");
 // read .env file before everything else
-dotenv.config();
+dotenv.config({path: '.env.beta'});
+//test
 // import my services afterwards
 // import { Config, DB } from "./service"
 const movie_repository_1 = require("./repository/movie.repository");
